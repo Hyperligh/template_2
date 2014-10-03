5e5e2a87f9bba74402a8412d982e8cfdd60ec36b
Hyperligh <hyperligh19@hotmail.com>
2014-10-02 21:31:41 -0700

    First start of coffee website

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<meta name="author" content="Josh Chambers">
+		<meta name="description" content="Best Coffee in Bend, OR">
+		<meta name="keywords" content="Coffee,Bend, OR, Bend OR, Coffee Bend OR">
+		<meta name="viewport" content="width=device-width">
+		<link rel="stylesheet" src="style/style1.css">
+	</head>
+<body>
+	<div id="container">
+		<header>
+			<nav>
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">About Us</a></li>
+					<li><a href="#">Types O Coffee</a></li>
+					<li><a href="#">Merchandise</a></li>
+					<li><a href="#">Contact Us</a></li>
+				</ul>
+			</nav>
+		</header>
+			<div id="content">
+
+
+			</div>
+
+
+	</div>
+</body>
+</html>
+
